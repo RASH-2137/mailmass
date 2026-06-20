26ebc9892bcb108679cb88f6c55c2f80021a6c87
Rahul Sharma <2137rahulsharma@gmail.com>
2026-06-20 21:16:33 +0530

    fixing SMTP issues

--- app/email/email_sender.py
+++ app/email/email_sender.py
@@ -3,12 +3,14 @@ from app.config import EMAIL_PROVIDER
 from app.email.providers.smtp_provider import SMTPProvider
 from app.email.providers.resend_provider import ResendProvider
 
+print("EMAIL PROVIDER =", EMAIL_PROVIDER)
 
 if EMAIL_PROVIDER == "resend":
+    print("USING RESEND PROVIDER")
     provider = ResendProvider()
 else:
+    print("USING SMTP PROVIDER")
     provider = SMTPProvider()
-
 
 def send_email(
     to_email: str,

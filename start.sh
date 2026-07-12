132e869a8d94174a6408690de0b58ffd201b7976
Rahul Sharma <2137rahulsharma@gmail.com>
2026-07-12 16:49:24 +0530

    Automate alembic migrations on startup

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
+set -e
 
 echo "Running database migrations..."
-
-python -m alembic upgrade head
+python3 -m alembic upgrade head
 
 echo "Starting FastAPI..."
-
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000

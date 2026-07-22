35d9e7fe93af44eb58fe454522ec0258b08a0186
Rahul Sharma <2137rahulsharma@gmail.com>
2026-07-22 11:51:44 +0530

    finishing

--- app/main.py
+++ app/main.py
@@ -43,9 +43,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://musky-unimpeded-cosigner.ngrok-free.dev",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "https://mailmass.onrender.com",
+        "https://mailmass-alpha.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],

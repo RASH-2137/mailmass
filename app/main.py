8c53699514968133da08d3086b1681f73a8ae005
Rahul Sharma <2137rahulsharma@gmail.com>
2026-07-23 02:51:38 +0530

    switching to custom domain

--- app/main.py
+++ app/main.py
@@ -43,8 +43,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://mailmass.onrender.com",
-        "https://mailmass-alpha.vercel.app"
+        "https://mailmass.spacekid.xyz"
     ],
     allow_credentials=True,
     allow_methods=["*"],
